69ca9ae3af548b053edec28d099d7ee818871c3f
saishkulkarni <saishkulkarni7@gmail.com>
2025-09-19 12:51:19 +0530

    Registration with OTP

--- src/main/java/com/m15/clicknbuy/util/OtpSender.java
+++ src/main/java/com/m15/clicknbuy/util/OtpSender.java
@@ -1,11 +1,38 @@
 package com.m15.clicknbuy.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class OtpSender {
+
+	@Autowired
+	JavaMailSender mailSender;
+
+	@Autowired
+	TemplateEngine templateEngine;
+
 	public void sendOtpThruEmail(String email, int otp, String name) {
-		System.out.println("Otp Sent to Email : " + email + " , OTP is : " + otp);
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		try {
+			helper.setFrom("noreply@clicknbuy.com", "Clicknbuy");
+			helper.setTo(email);
+			helper.setSubject("Otp for Creating account with ClickNBuy");
+			Context context = new Context();
+			context.setVariable("name", name);
+			context.setVariable("otp", otp);
+			String emailMessage = templateEngine.process("email-template.html", context);
+			helper.setText(emailMessage, true);
+			mailSender.send(message);
+		} catch (Exception e) {
+		}
 	}
 
 	public void sendOtpThruMobile(Long mobile, int otp, String name) {
